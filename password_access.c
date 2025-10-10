8e90cc9e4b050c973ffd214fc758dbae07911064
Jezryl☆! <jezryllumbasi001@gmail.com>
2025-10-10 12:12:00 +0300

    Add files via upload

---
+++ password_access.c
@@ -0,0 +1,21 @@
+/*
+NAME:JEZRYL LUMBASI 
+REG NO:CT101/G/26464/25
+DESCRIPTION:PASSWORD ACCESS
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char password[20];
+
+    do {
+        printf("Enter password: ");
+        scanf("%s", password);
+    } while (strcmp(password, "1234") != 0);
+
+    printf("Access Granted\n");
+
+    return 0;
+}
