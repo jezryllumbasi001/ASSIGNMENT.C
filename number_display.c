0641153db966a9c1de197f1ac74a7169f4c31c76
Jezryl☆! <jezryllumbasi001@gmail.com>
2025-10-10 12:04:26 +0300

    Add files via upload

---
+++ number_display.c
@@ -0,0 +1,14 @@
+/*
+NAME:JEZRYL LUMBASI 
+REG NO:CT101/G/26464/25
+DESCRIPTION:NUMBER DISPLAY
+*/
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
