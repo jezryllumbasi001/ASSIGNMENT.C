53e85d53e77f355ccc187cccc13d050d67d577f5
Jezryl☆! <jezryllumbasi001@gmail.com>
2025-10-23 23:33:32 +0300

    Add files via upload

---
+++ calculate_fare.c
@@ -0,0 +1,23 @@
+/*
+NAME:JEZRYL LUMBASI 
+REG NO:CT101/G/26464/25
+DESCRIPTION:calculate fare
+*/
+#include <stdio.h>
+
+//prompting user to calculate total fare
+int calculateFare(int distance) {
+    int fare = distance * 50;
+    return fare;
+}
+//prompt user to enter distance traveled in km
+int main() {
+    int distance;
+    printf("Enter distance traveled (in km): ");
+    scanf("%d", &distance);
+
+    int totalFare = calculateFare(distance);
+    printf("Total fare = KSh. %d\n", totalFare);
+
+    return 0;
+}
