6ec7de47c6d47018c35a3575359acc51547bca59
Jezryl☆! <jezryllumbasi001@gmail.com>
2025-10-23 23:37:19 +0300

    Add files via upload

---
+++ convert_to_celcius.c
@@ -0,0 +1,24 @@
+/*
+NAME:JEZRYL LUMBASI 
+REG NO:CT101/G/26464/25
+DESCRIPTION:convert to celsius
+*/
+#include <stdio.h>
+
+// prompting user to convert Fahrenheit to Celsius
+float convertToCelsius(float fahrenheit) {
+    float celsius = (fahrenheit - 32) * 5 / 9;
+    return celsius;
+}
+
+//prompting user to enter temperature in Fahrenheit 
+int main() {
+    float fahrenheit;
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+
+    float celsius = convertToCelsius(fahrenheit);
+    printf("Temperature in Celsius = %.1f°C\n", celsius);
+
+    return 0;
+}
